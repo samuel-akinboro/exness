9e756aa0c1b47f52a7756ea330ab67f7ddb5ea83
samuel-akinboro <victorakinboro2003@gmail.com>
2023-09-21 12:50:57 +0100

    changed form color

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -3,20 +3,20 @@
 import Link from 'next/link'
 import { useEffect } from 'react'
 
-export default function Home() {
+export default function SignUp() {
   useEffect(() => {
     import('preline')
   }, [])
 
   return (
-    <div className="bg-black/90 flex min-h-screen items-center py-16">
+    <div className="bg-gray3 flex min-h-screen items-center py-16">
       <main className="w-full max-w-md mx-auto p-6">
-        <div className="mt-7 border rounded-xl shadow-sm bg-black/10 border-gray-700">
+        <div className="mt-7 border rounded-xl shadow-sm bg-white">
           <div className="p-4 sm:p-7">
             <div className="text-center mb-4">
-              <h1 className="block text-2xl font-bold text-white">Sign up</h1>
-              <p className="mt-2 text-sm text-gray-400">
-                Already have an account?
+              <h1 className="block text-2xl font-bold text-black">Sign up</h1>
+              <p className="mt-2 text-sm text-gray-600">
+                Already have an account?{" "}
                 <Link className="text-secondary decoration-2 hover:underline font-medium" href="/sign-in">
                   Sign in here
                 </Link>
@@ -29,9 +29,9 @@ export default function Home() {
                 <div className="grid gap-y-4">
                   {/* <!-- Form Group --> */}
                   <div>
-                    <label for="email" className="block text-sm mb-2 text-white">Full Name</label>
+                    <label for="email" className="block text-sm mb-2 text-black">Full Name</label>
                     <div className="relative">
-                      <input type="text" id="name" name="name" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-[#E7F0FE] border-gray-700 text-gray-900" required aria-describedby="name-error" />
+                      <input type="text" id="name" name="name" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-white border border-gray-700 text-gray-900" required aria-describedby="name-error" />
                       <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -40,9 +40,9 @@ export default function Home() {
                     </div>
                   </div>
                   <div>
-                    <label for="email" className="block text-sm mb-2 text-white">Email address</label>
+                    <label for="email" className="block text-sm mb-2 text-black">Email address</label>
                     <div className="relative">
-                      <input type="email" id="email" name="email" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-[#E7F0FE] border-gray-700 text-gray-900" required aria-describedby="email-error" />
+                      <input type="email" id="email" name="email" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-white border border-gray-700 text-gray-900" required aria-describedby="email-error" />
                       <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -55,9 +55,9 @@ export default function Home() {
 {/* 
                   <!-- Form Group --> */}
                   <div>
-                    <label for="password" className="block text-sm mb-2 text-white">Password</label>
+                    <label for="password" className="block text-sm mb-2 text-black">Password</label>
                     <div className="relative">
-                      <input type="password" id="password" name="password" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-[#E7F0FE] border-gray-700 text-gray-900" required aria-describedby="password-error" />
+                      <input type="password" id="password" name="password" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-white border border-gray-700 text-gray-900" required aria-describedby="password-error" />
                       <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -70,9 +70,9 @@ export default function Home() {
 
                   {/* <!-- Form Group --> */}
                   <div>
-                    <label for="confirm-password" className="block text-sm mb-2 text-white">Confirm Password</label>
+                    <label for="confirm-password" className="block text-sm mb-2 text-black">Confirm Password</label>
                     <div className="relative">
-                      <input type="password" id="confirm-password" name="confirm-password" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-[#E7F0FE] border-gray-700 text-gray-900" required aria-describedby="confirm-password-error" />
+                      <input type="password" id="confirm-password" name="confirm-password" className="py-3 px-4 block w-full rounded-md text-sm focus:border-secondary focus:ring-secondary bg-white border border-gray-700 text-gray-900" required aria-describedby="confirm-password-error" />
                       <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
                         <svg className="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
@@ -89,7 +89,7 @@ export default function Home() {
                       <input id="remember-me" name="remember-me" type="checkbox" className="shrink-0 mt-0.5 rounded text-secondary pointer-events-none focus:ring-secondary bg-gray-800 border-gray-700 checked:bg-secondary checked:border-secondary focus:ring-offset-gray-800" />
                     </div>
                     <div className="ml-3">
-                      <label for="remember-me" className="text-sm text-white">I accept the <a className="text-secondary decoration-2 hover:underline font-medium" href="#">Terms and Conditions</a></label>
+                      <label for="remember-me" className="text-sm text-black">I accept the <a className="text-secondary decoration-2 hover:underline font-medium" href="#">Terms and Conditions</a></label>
                     </div>
                   </div>
                   {/* <!-- End Checkbox --> */}
